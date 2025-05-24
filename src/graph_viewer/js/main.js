86e1412e599f010446b467de5958a19e729d8e5d
ShiotaTakumi <shiotatakumi1009@outlook.jp>
2025-05-24 21:48:03 +0200

    add: add additional json data for prism variant 1

--- src/graph_viewer/js/main.js
+++ src/graph_viewer/js/main.js
@@ -60,6 +60,14 @@ const dataMap = {
     n9: Array.from({ length: 20 }, (_, i) => `${i + 1}.json`),
     n10: Array.from({ length: 34 }, (_, i) => `${i + 1}.json`),
     n11: Array.from({ length: 54 }, (_, i) => `${i + 1}.json`)
+  },
+  prism_var_1: {
+    n6: Array.from({ length: 1 }, (_, i) => `${i + 1}.json`),
+    n7: Array.from({ length: 2 }, (_, i) => `${i + 1}.json`),
+    n8: Array.from({ length: 2 }, (_, i) => `${i + 1}.json`),
+    n9: Array.from({ length: 3 }, (_, i) => `${i + 1}.json`),
+    n10: Array.from({ length: 3 }, (_, i) => `${i + 1}.json`),
+    n11: Array.from({ length: 4 }, (_, i) => `${i + 1}.json`)
   }
 };
 
