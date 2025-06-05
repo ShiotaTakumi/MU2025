99dfea294241e9b536b416dd2942f4734f1475f3
ShiotaTakumi <shiotatakumi1009@outlook.jp>
2025-06-05 21:03:14 +0200

    add: add additional json data for pyramid variant 7

--- src/graph_viewer/js/main.js
+++ src/graph_viewer/js/main.js
@@ -104,6 +104,11 @@ const dataMap = {
     n9: Array.from({ length: 1 }, (_, i) => `${i + 1}.json`),
     n10: Array.from({ length: 1 }, (_, i) => `${i + 1}.json`),
     n11: Array.from({ length: 1 }, (_, i) => `${i + 1}.json`)
+  },
+  pyramid_var_7: {
+    n6: Array.from({ length: 1 }, (_, i) => `${i + 1}.json`),
+    n8: Array.from({ length: 2 }, (_, i) => `${i + 1}.json`),
+    n10: Array.from({ length: 3 }, (_, i) => `${i + 1}.json`)
   }
 };
 
